20eb1bf7b450f4e3486c93e886cca5f2315b34bf
yv-hua-qian <139928687+yv-hua-qian@users.noreply.github.com>
2025-02-16 21:40:13 +0800

    仿淘宝项目立项

---
+++ src/main/java/yv/hua/taobaofang/TaoBaoFangApplication.java
@@ -0,0 +1,13 @@
+package yv.hua.taobaofang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaoBaoFangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaoBaoFangApplication.class, args);
+    }
+
+}
